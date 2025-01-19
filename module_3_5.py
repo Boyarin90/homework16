4df4fb3c71574674609073b2ddcfd63405f2e90c
Boyarin90 <lomoti1990@gmail.com>
2025-01-19 14:58:53 +0900

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -5,7 +5,7 @@ def get_multiplied_digits (number):
         return (first * get_multiplied_digits
         (int(str_number[1:])))
     else:
-        return first
+        return first if first!=0 else 1
 
 result = get_multiplied_digits(40203)
 result2 = get_multiplied_digits(402030)
